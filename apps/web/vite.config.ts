0c7b1f2ddd1abc335358b2b7db7777a07538c089
Eiinu <eiinu@qq.com>
2025-10-15 01:27:51 +0800

    fix: build error

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    preserveSymlinks: true
-  },
-  optimizeDeps: {
-    exclude: ['@eiinu/editor', '@eiinu/tools', '@eiinu/creative']
-  },
-  server: {
-    fs: {
-      // 允许访问整个工作区
-      allow: [resolve(__dirname, '../../')]
-    }
-  }
+  plugins: [react()]
 })
